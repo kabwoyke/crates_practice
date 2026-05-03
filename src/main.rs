bda4df1de2c61daf9db8e3170193bdb7aa73fecb
kabwoyke <edwardkabwoy@gmail.com>
2026-05-03 09:12:36 +0300

    Revert "adding a rectangle to the screen"
    This reverts commit 657571ed042ae05b2bc3f8c253cc9e8447ec3c92.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
     let mut app = simple::Window::new("Example App", 1920, 1080);
     app.set_color(200, 100, 50, 255);
-    app.draw_rect(simple::Rect::new(110, 210, 900, 400));
     while app.next_frame(){}
 }
